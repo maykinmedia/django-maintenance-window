31ce2bfac3641389d70c3092df2f07e724d81764
Conor Holden <conor@maykinmedia.nl>
2025-05-16 14:50:34 +0200

    :white_check_mark: add middleware settings tests

--- tests/settings.py
+++ tests/settings.py
@@ -42,6 +42,7 @@ TEMPLATES = [
 INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.sessions",
     "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.admin",
